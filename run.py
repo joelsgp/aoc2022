6613dda06ad5738c24e64b453a6ec273628ef080
jmcb <joelsgp@protonmail.com>
2022-12-06 06:41:47 +0000

    Try alternate base solution system

---
+++ run.py
@@ -0,0 +1,81 @@
+from argparse import ArgumentParser
+from datetime import datetime, timedelta, timezone
+from pathlib import Path
+from typing import Callable
+from sys import stderr
+
+import solutions
+
+try:
+    import pyperclip
+except ImportError:
+    pyperclip = None
+
+
+INPUTS_DIR_NAME = 'inputs'
+INPUT_SUFFIX = '.txt'
+# when problems are released, relative to UTC
+RELEASE_OFFSET = timedelta(hours=-3)
+RELEASE_TZ = timezone(RELEASE_OFFSET)
+
+Lines = list[str]
+
+
+def get_parser() -> ArgumentParser:
+    parser = ArgumentParser()
+
+    parser.add_argument('day', type=int, required=False)
+    parser.add_argument('-c', '--copy', action='store_false', help='Copy solution to clipboard')
+
+    return parser
+
+
+def load_lines(filename: str) -> Lines:
+    """Note: uses str.splitlines."""
+    inputs_dir = Path(INPUTS_DIR_NAME)
+    filepath = inputs_dir / filename
+    filepath = filepath.with_suffix(INPUT_SUFFIX)
+    text = filepath.read_text(encoding='utf-8')
+    lines = text.splitlines()
+
+    return lines
+
+
+def current_day() -> int:
+    now = datetime.now(tz=RELEASE_TZ)
+    day = now.day
+    return day
+
+
+def retrieve_solver(filename: str) -> Callable:
+    try:
+        module = getattr(solutions, filename)
+    except AttributeError as error:
+        raise AttributeError("Solution doesn't exist!") from error
+
+    return module.solve
+
+
+def format_filename(day: int) -> str:
+    return f'{day:02}'
+
+
+def main():
+    parser = get_parser()
+    args = parser.parse_args()
+
+    if args.day is None:
+        args.day = current_day()
+    filename = format_filename(args.day)
+
+    solve = retrieve_solver(filename)
+    lines = load_lines(filename)
+
+    solution = solve(lines)
+
+    if args.copy:
+        if pyperclip is None:
+            print("pyperclip isn't installed, won't copy solution", file=stderr)
+        else:
+            pyperclip.copy(solution)
+    print(solution)
---
+++ solutions/01.py
@@ -0,0 +1,16 @@
+def solve(lines):
+    sums = []
+    current_sum = 0
+    for li in lines:
+        try:
+            current_sum += int(li.strip())
+        except ValueError:
+            sums.append(current_sum)
+            current_sum = 0
+
+
+    sums.sort(reverse=True)
+    top3 = sums[:3]
+    total = sum(top3)
+
+    return total
