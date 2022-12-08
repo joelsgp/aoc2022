73d112f6d1c4552b4b9efe028d471fbca7b446e7
jmcb <joelsgp@protonmail.com>
2022-12-08 23:04:26 +0000

    6-1

---
+++ solutions/d06.py
@@ -0,0 +1,13 @@
+MARKER_SIZE = 4
+
+
+def solve(lines):
+    buffer = lines.pop()
+
+    last4 = list(buffer[:MARKER_SIZE])
+    for i, char in enumerate(buffer[MARKER_SIZE:]):
+        if len(set(last4)) == MARKER_SIZE:
+            return i + MARKER_SIZE
+
+        last4.pop(0)
+        last4.append(char)
