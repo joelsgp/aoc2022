40388cca3ebdbabb437dad8cc5ec6f14b954764b
jmcb <joelsgp@protonmail.com>
2022-12-08 21:33:43 +0000

    4-2-final

--- solutions/d04.py
+++ solutions/d04.py
@@ -5,8 +5,13 @@ from itertools import permutations
 PAIR = re.compile(r'(\d+)-(\d+),(\d+)-(\d+)')
 
 
+# could do this more lazily with ranges and sets, but it would be less efficient than just checking bounds
 def slice_encompass(*s: slice) -> bool:
     return any(a.start >= b.start and a.stop <= b.stop for a, b in permutations(s, 2))
+
+
+def slice_overlap(*s: slice) -> bool:
+    return any(a.start <= b.start <= a.stop for a, b in permutations(s, 2))
 
 
 def solve(lines):
@@ -15,6 +20,6 @@ def solve(lines):
         m = PAIR.fullmatch(li)
         x = slice(int(m[1]), int(m[2]))
         y = slice(int(m[3]), int(m[4]))
-        if slice_encompass(x, y):
+        if slice_overlap(x, y):
             total += 1
     return total
