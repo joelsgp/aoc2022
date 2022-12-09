fd55f18da41470e82e49a044f7d6e96f5362e05a
jmcb <joelsgp@protonmail.com>
2022-12-09 00:36:09 +0000

    7-1-wip1

---
+++ solutions/d07.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+from re import compile
+from typing import Optional
+
+
+Lines = list[str]
+
+RE_COMMAND_LS = compile(r'\$ ls')
+RE_COMMAND_CD = compile(r'\$ cd (.+)')
+RE_OUTPUT_DIR = compile('dir (.+)')
+RE_OUTPUT_FILE = compile('([0-9]+) (.+)')
+
+
+class Node:
+    def __init__(self, name: str, parent: Optional[Directory]):
+        self.name = name
+        self.parent = parent
+        if self.parent is not None:
+            self.parent.children.append(self)
+
+
+class File(Node):
+    def __init__(self, name: str, parent: Directory, size: int):
+        super().__init__(name, parent)
+        self.size = size
+
+
+class Directory(Node):
+    def __init__(self, name: str, parent: Optional[Directory]):
+        super().__init__(name, parent)
+        self.children: list[Node] = []
+
+
+def process_cd(name: str, pwd: Directory, root: Directory) -> Directory:
+    if name == '/':
+        return root
+    elif name == '..':
+        if pwd.parent is not None:
+            return pwd.parent
+        else:
+            return pwd
+    else:
+        for n in pwd.children:
+            if n.name == name and isinstance(n, Directory):
+                return n
+        new = Directory(name, pwd)
+        return new
+
+
+def process_ls(output: Lines, pwd: Directory):
+    for line in output:
+        if m := RE_OUTPUT_DIR.fullmatch(line):
+            name = m[1]
+            Directory(name, pwd)
+        elif m := RE_OUTPUT_FILE.fullmatch(line):
+            size = m[1]
+            name = m[2]
+            File(name, pwd, size)
+
+
+def process_command(command: str, output: Lines, pwd: Directory, root: Directory) -> Directory:
+    if m := RE_COMMAND_CD.fullmatch(command):
+        pwd = process_cd(m[1], pwd, root)
+    elif RE_COMMAND_LS.fullmatch(command):
+        process_ls(output, pwd)
+    return pwd
+
+
+def solve(lines):
+    root = Directory('', None)
+    pwd = root
+
+    command = lines[0]
+    output = []
+    for li in lines[1:]:
+        if li.startswith('$'):
+            pwd = process_command(command, output, pwd, root)
+            command = li
+            output = []
+        else:
+            output.append(li)
