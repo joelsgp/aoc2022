9c1d1143dfcc1871a245c0ba27afe4a312b958f6
jmcb <joelsgp@protonmail.com>
2022-12-09 08:29:53 +0000

    Revert "8-1-wiphmmm"
    This reverts commit 0601550b6404e58f4bb9a8469d4de25524977050.

--- solutions/d08.py
+++ solutions/d08.py
@@ -1,7 +1,19 @@
 def solve(lines):
     total_visible = 0
 
+    grid_height = len(lines)
+    grid_width = len(lines[0])
+    visibility_grid = [[None] * grid_width for _ in range(grid_height)]
+    for i in (0, -1):
+        visibility_grid[i] = [True] * grid_width
+        for row in visibility_grid:
+            row[i] = True
+    total_visible += grid_height * 2 + grid_width * 2 - 4
+
     for y, row in enumerate(lines):
+        # currently hitting the grid[y]
+        visibility_row = visibility_grid[y]
+
         for x, column in enumerate(row):
             pass
 
